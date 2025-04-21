971142c060980a41647d79c26d1bc88543f10463
Chanz Langreo <langreo.grim@gmail.com>
2025-04-21 15:05:11 +0800

    add create new db if not

--- src/lib/db_interface.py
+++ src/lib/db_interface.py
@@ -1,11 +1,43 @@
 """
 Hello
 """
-from PIL import Image
 import sqlite3
-from lib import img_manip
+from os import path
 
-con = sqlite3.connect('../faces.db')
+db_path = 'src/faces.db'
+
+if not path.exists(db_path):
+    print('Database file doesn\'t exist, creating it now.')
+    open(db_path, 'w')
+    con = sqlite3.connect(db_path)    
+    cur = con.cursor()
+
+    cur.execute("""
+                CREATE TABLE "courses" (
+                "course"	TEXT,
+                PRIMARY KEY("course")
+                );
+                """)
+    
+    con.commit()
+    
+    cur.execute("""
+                CREATE TABLE "registered_students" (
+                "id"	INTEGER,
+                "name"	TEXT NOT NULL,
+                "face_encodings"	BLOB NOT NULL,
+                "course"	TEXT NOT NULL,
+                "year"	INTEGER NOT NULL,
+                FOREIGN KEY("course") REFERENCES "courses"("course") ON UPDATE CASCADE,
+                PRIMARY KEY("id")
+            );
+                """)
+    
+    con.commit()
+
+
+
+con = sqlite3.connect(db_path)
 cur = con.cursor()
 
 
@@ -27,13 +59,16 @@ def insert_into_db(binary_img, id : int, name : str, course : str, year : int) -
     
     cur.execute("INSERT INTO registered_students VALUES(?, ?, ?, ?, ?)", data_to_db)
 
+    con.commit()
+    con.close()
+
 def pull_from_db(id : int, values : tuple | None = None):
     if values:
         res = cur.execute(f"SELECT {values} FROM registered_students WHERE id={id}")
     else:
         res = cur.execute(f"SELECT * FROM registered_students WHERE id={id}")
+        
+    con.commit()
+    con.close()
+    
     return res.fetchone()
-
-
-con.commit()
-con.close()
